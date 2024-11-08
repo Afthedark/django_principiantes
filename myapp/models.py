7dd9bb1ffe508169f451bdf163f2d0dfa8b8cb0a
Luis Gonzalo <afthedark@hotmail.com>
2024-11-08 18:01:37 -0400

    cont_params

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+
+"""
+El borrado en cascada en SQL es una característica que permite eliminar automáticamente 
+registros relacionados cuando se elimina una fila de la tabla principal. Esto ayuda a mantener 
+la integridad de la base de datos y la consistencia de los datos. 
+ 
+Para especificar el borrado en cascada, se utiliza la cláusula ON DELETE CASCADE. Por ejemplo, 
+si se elimina un cliente de la tabla Clientes, se eliminarán todas las filas que contengan el 
+mismo valor de identificador de cliente. 
+ 
+
+"""
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) #Se relaciona con foreign key de Project y se elimina en cascada 
+    
+     
--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+#Agregar las aplicaciones creadas   
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
